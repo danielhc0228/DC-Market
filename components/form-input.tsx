f78ccec9d1dfdcb8c54e1f98bc3dbd1287d54cb7
Daniel Chung <daniel20020228@gmail.com>
2025-05-01 15:25:33 +1000

    Add Server Action function in login page. This one function does all the work which doesn't require route handler like in the previous commit. use server at the top of the function and code below is run to get sent to the server

--- components/form-input.tsx
+++ components/form-input.tsx
@@ -3,12 +3,14 @@ interface FormInputProps {
     placeholder: string;
     required: boolean;
     errors: string[];
+    name: string;
 }
 
-export default function FormInput({ type, placeholder, required, errors }: FormInputProps) {
+export default function FormInput({ type, placeholder, required, errors, name }: FormInputProps) {
     return (
         <div className="flex flex-col gap-2">
             <input
+                name={name}
                 className="h-10 w-full rounded-md border-none bg-transparent pl-5 ring-2 ring-neutral-200 transition placeholder:text-neutral-400 focus:ring-3 focus:ring-orange-500 focus:outline-none"
                 type={type}
                 placeholder={placeholder}
