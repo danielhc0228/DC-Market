f3f0c72c49f421970e4f3286653686065fb78df6
Daniel Chung <daniel20020228@gmail.com>
2025-05-01 12:56:16 +1000

    Create create-account page

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Home() {
     return (
-        <div className='flex flex-col items-center justify-between min-h-screen p-6'>
+        <div className='flex flex-col items-center justify-between min-h-screen'>
             <div className='my-auto flex flex-col items-center gap-2 *:font-medium'>
                 <span className='text-9xl'>🥕</span>
                 <h1 className='text-4xl '>당근</h1>
@@ -11,7 +11,7 @@ export default function Home() {
             <div className='flex flex-col items-center gap-3 w-full'>
                 <Link
                     href='/create-account'
-                    className='w-full bg-orange-500 text-white text-lg font-medium py-2.5 rounded-md text-center hover:bg-orange-400 transition-colors'
+                    className='primary-btn text-lg py-2.5'
                 >
                     시작하기
                 </Link>
