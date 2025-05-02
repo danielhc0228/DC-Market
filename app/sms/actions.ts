4ced43b8568c3dabb03f548ee48d9da8a739f222
Daniel Chung <daniel20020228@gmail.com>
2025-05-02 17:16:25 +1000

    update sms page.tsx to send phone number and verification code and update actions.ts to handle parsed phone number and token using zod schema.

--- app/sms/actions.ts
+++ app/sms/actions.ts
@@ -2,9 +2,51 @@
 
 import { z } from "zod";
 import validator from "validator";
+import { redirect } from "next/navigation";
 
-const phoneSchema = z.string().trim().refine(validator.isMobilePhone);
+const phoneSchema = z
+    .string()
+    .trim()
+    .refine(
+        (phone) => validator.isMobilePhone(phone, "en-AU"), //restricts to Australian mobile phone number
+        "Wrong phone format",
+    );
 
 const tokenSchema = z.coerce.number().min(100000).max(999999); // token is string initially but 'coerce' forces type conversion to number.
 
-export async function smsLogIn(prevState: unknown, formData: FormData) {}
+interface ActionState {
+    token: boolean;
+}
+
+export async function smsLogIn(prevState: ActionState, formData: FormData) {
+    const phone = formData.get("phone");
+    const token = formData.get("token");
+    if (!prevState.token) {
+        // if token is false (initially set to false), check for valid phone number
+        const result = phoneSchema.safeParse(phone);
+        // if invalid, show error
+        if (!result.success) {
+            return {
+                token: false,
+                error: result.error.flatten(),
+            };
+        } else {
+            //else set token to true
+            return {
+                token: true,
+            };
+        }
+    } else {
+        // this code is run when the button is clicked again but this time token is set to true so it can be run.
+        const result = tokenSchema.safeParse(token);
+        if (!result.success) {
+            //show error when verification code is incorrect
+            return {
+                token: true,
+                error: result.error.flatten(),
+            };
+        } else {
+            redirect("/"); //redirect to home screen if verfication code is correct
+        }
+    }
+}
--- app/sms/page.tsx
+++ app/sms/page.tsx
@@ -1,10 +1,17 @@
+"use client";
+
 import Button from "@/components/button";
 import Input from "@/components/input";
 import { smsLogIn } from "./actions";
 import { useActionState } from "react";
 
+const initialState = {
+    token: false,
+    error: undefined,
+};
+
 export default function SMSLogin() {
-    const [state, dispatch] = useActionState(smsLogIn, null);
+    const [state, dispatch] = useActionState(smsLogIn, initialState);
     return (
         <div className="flex flex-col gap-10 px-6 py-8">
             <div className="flex flex-col gap-2 *:font-medium">
@@ -12,17 +19,26 @@ export default function SMSLogin() {
                 <h2 className="text-xl">Verify your phone number.</h2>
             </div>
             <form action={dispatch} className="flex flex-col gap-3">
-                <Input name="phone" type="number" placeholder="Phone number" required errors={[]} />
-                <Input
-                    name="verfication"
-                    type="number"
-                    placeholder="Verification code"
-                    required
-                    min={100000}
-                    max={999999}
-                    // errors={[]}
-                />
-                <Button text="Verify" />
+                {state.token ? ( //if phone number is valid then show input for verfication code
+                    <Input
+                        name="token"
+                        type="number"
+                        placeholder="Verification code"
+                        required
+                        min={100000}
+                        max={999999}
+                    />
+                ) : (
+                    // else stay with phone number input
+                    <Input
+                        name="phone"
+                        type="text"
+                        placeholder="Phone number"
+                        required
+                        errors={state.error?.formErrors}
+                    />
+                )}
+                <Button text={state.token ? "Verify Token" : "Send Verification SMS"} />
             </form>
         </div>
     );
