b7847f625bdd5d68673b3f2181bfb98844bcf2d0
Daniel Chung <daniel20020228@gmail.com>
2025-05-06 14:41:54 +1000

    Delete prisma folder in lib and set directory for new prisma folder in node_modules in schema.prisma and add test functions in db.ts that creates and searches sms tokens

--- lib/db.ts
+++ lib/db.ts
@@ -1,5 +1,34 @@
-import { PrismaClient } from "./generated/prisma";
+import { PrismaClient } from "@prisma/client";
 
 const db = new PrismaClient();
+
+// async function test() { // this function creates token by searching and connecting user with id 3.
+//     const token = await db.sMSToken.create({
+//         data: {
+//             token: "121212",
+//             user: {
+//                 connect: {
+//                     id: 3,
+//                 },
+//             },
+//         },
+//     });
+//     console.log(token);
+// }
+
+async function test() {
+    // this function finds token based on the condition and includes user information e.g. id 1
+    const token = await db.sMSToken.findUnique({
+        where: {
+            id: 1,
+        },
+        include: {
+            user: true,
+        },
+    });
+    console.log(token);
+}
+
+test();
 
 export default db;
