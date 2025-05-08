27ad608df55eaa99df90d592adf8fbba440519d2
Daniel Chung <daniel20020228@gmail.com>
2025-05-08 14:22:42 +1000

    Update profile page. add log out button that destroys logged user's cookie and redirects to home page.

--- app/login/actions.ts
+++ app/login/actions.ts
@@ -59,6 +59,7 @@ export async function logIn(prevState: unknown, formData: FormData) {
         if (ok) {
             const session = await getSession();
             session.id = user!.id;
+            await session.save();
             redirect("/profile");
         } else {
             return {
