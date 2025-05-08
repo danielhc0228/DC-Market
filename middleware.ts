56285b3c3fd2dda9fef496cc52784118621b14b3
Daniel Chung <daniel20020228@gmail.com>
2025-05-08 15:19:07 +1000

    Add if else statement to prevent logged out users access private urls in middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,35 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+import getSession from "./lib/session";
+
+interface Routes {
+    [key: string]: boolean;
+}
+
+const publicOnlyUrls: Routes = {
+    "/": true,
+    "/login": true,
+    "/sms": true,
+    "/create-account": true,
+};
+
+// const publicUrls = new Set(['/', '/login', '/sms', '/create-account']); // Set is a good option too
 
 export async function middleware(request: NextRequest) {
-    console.log("hello");
+    const session = await getSession();
+    const exists = publicOnlyUrls[request.nextUrl.pathname];
+    if (!session.id) {
+        if (!exists) {
+            return NextResponse.redirect(new URL("/", request.url)); // if user is logged out and tries to access private url, redirect to home page
+        }
+    } else {
+        if (exists) {
+            return NextResponse.redirect(new URL("/products", request.url)); // else, send user to product page (for now).
+        }
+    }
 }
 
 export const config = {
-    matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"], //middleware only runs where matcher specifiy. In this case, only run when there is no /api, /_next/static etc.
+    matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+    //middleware only runs where matcher specifiy. In this case, only run when there is no /api, /_next/static etc.
+    // ["/", "/profile", "/create-account"] <- only runs in those address
 };
