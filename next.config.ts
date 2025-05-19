a8f088e1e6059037a5a520fa9f6255aaaf07699c
Daniel Chung <daniel20020228@gmail.com>
2025-05-19 15:18:53 +1000

    Modify product[id] page to have its photos, description and user who posted the product and buttons that allow users to chat with the owner.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    /* config options here */
+};
+
+// next.config.js
+module.exports = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "avatars.githubusercontent.com",
+                port: "",
+                pathname: "/**",
+            },
+        ],
+    },
 };
 
 export default nextConfig;
