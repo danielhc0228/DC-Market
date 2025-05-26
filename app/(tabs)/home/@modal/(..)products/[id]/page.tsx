8a24325f9779eba86328b5ed789f976c4531ebe4
Daniel Chung <daniel20020228@gmail.com>
2025-05-26 15:00:03 +1000

    Create a separate component for close button in modal to use use client and use server separately in one modal. CloseButton require use client

--- app/(tabs)/home/@modal/(..)products/[id]/page.tsx
+++ app/(tabs)/home/@modal/(..)products/[id]/page.tsx
@@ -1,18 +1,12 @@
-"use client";
+"use server";
 
-import { PhotoIcon, XMarkIcon } from "@heroicons/react/24/solid";
-import { useRouter } from "next/navigation";
+import CloseButton from "@/components/close-button";
+import { PhotoIcon } from "@heroicons/react/24/solid";
 
-export default function Modal({ params }: { params: { id: string } }) {
-    const router = useRouter();
-    const onCloseClick = () => {
-        router.back();
-    };
+export default async function Modal({ params }: { params: { id: string } }) {
     return (
         <div className="bg-opacity-80 absolute top-0 left-0 z-50 flex h-full w-full items-center justify-center bg-black/75">
-            <button onClick={onCloseClick} className="absolute top-5 right-5 text-neutral-200">
-                <XMarkIcon className="size-10" />
-            </button>
+            <CloseButton />
             <div className="flex h-1/2 w-full max-w-screen-sm justify-center">
                 <div className="flex aspect-square items-center justify-center rounded-md bg-neutral-700 text-neutral-200">
                     <PhotoIcon className="h-28" />
