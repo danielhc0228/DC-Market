4a98b9a0129e8e748cd78d297c71349657ef7956
Daniel Chung <daniel20020228@gmail.com>
2025-07-14 17:20:37 +1000

    Change css of add post page

--- app/(tabs)/life/add/page.tsx
+++ app/(tabs)/life/add/page.tsx
@@ -7,26 +7,42 @@ import Button from "@/components/button";
 
 export default function AddPost() {
     const [state, action] = useActionState(uploadPost, null);
+
     return (
-        <div>
-            Add New Post
-            <form action={action}>
-                <Input
-                    name="title"
-                    required
-                    placeholder="Title"
-                    type="text"
-                    errors={state?.fieldErrors.title}
-                ></Input>
-                <Input
-                    name="description"
-                    required
-                    placeholder="Description"
-                    type="text"
-                    errors={state?.fieldErrors.description}
-                ></Input>
-                <Button text="Submit" />
-            </form>
+        <div className="flex min-h-screen justify-center px-1 py-8">
+            <div className="w-full max-w-full rounded-2xl p-4 shadow-lg">
+                <h2 className="mb-6 text-center text-2xl font-bold">Add New Post</h2>
+
+                <form action={action} className="space-y-5">
+                    <div>
+                        <label htmlFor="title" className="mb-1 block text-sm font-medium">
+                            Title
+                        </label>
+                        <Input
+                            name="title"
+                            required
+                            placeholder="Post Title"
+                            type="text"
+                            errors={state?.fieldErrors.title}
+                        />
+                    </div>
+
+                    <div>
+                        <label htmlFor="description" className="mb-1 block text-sm font-medium">
+                            Description
+                        </label>
+                        <Input
+                            name="description"
+                            required
+                            placeholder="Write something..."
+                            type="text"
+                            errors={state?.fieldErrors.description}
+                        />
+                    </div>
+
+                    <Button text="Submit" />
+                </form>
+            </div>
         </div>
     );
 }
