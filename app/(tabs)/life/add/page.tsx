abe3093eca6539a994841cd95a4a14e2136d5bde
Daniel Chung <daniel20020228@gmail.com>
2025-07-14 17:06:48 +1000

    Add useActionState to apply actions.ts and upload data to database in add post page

--- app/(tabs)/life/add/page.tsx
+++ app/(tabs)/life/add/page.tsx
@@ -1,27 +1,31 @@
+"use client";
+
 import Input from "@/components/input";
-// import { useActionState } from "react";
+import { useActionState } from "react";
+import { uploadPost } from "./actions";
+import Button from "@/components/button";
 
 export default function AddPost() {
-    // const [state, action] = useActionState({}, null);
+    const [state, action] = useActionState(uploadPost, null);
     return (
         <div>
             Add New Post
-            {/* <form action={action}> */}
-            <form>
+            <form action={action}>
                 <Input
                     name="title"
                     required
                     placeholder="Title"
                     type="text"
-                    // errors={state?.fieldErrors.title}
+                    errors={state?.fieldErrors.title}
                 ></Input>
                 <Input
                     name="description"
                     required
                     placeholder="Description"
                     type="text"
-                    // errors={state?.fieldErrors.description}
+                    errors={state?.fieldErrors.description}
                 ></Input>
+                <Button text="Submit" />
             </form>
         </div>
     );
