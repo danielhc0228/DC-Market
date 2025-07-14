d4c7ee14c5c4402f423fcf282ee863c9c79f5430
Daniel Chung <daniel20020228@gmail.com>
2025-07-14 16:55:12 +1000

    Add form and inputs in add post page

--- app/(tabs)/life/add/page.tsx
+++ app/(tabs)/life/add/page.tsx
@@ -1,8 +1,28 @@
-export default async function AddPost() {
+import Input from "@/components/input";
+// import { useActionState } from "react";
+
+export default function AddPost() {
+    // const [state, action] = useActionState({}, null);
     return (
         <div>
             Add New Post
-            <form></form>
+            {/* <form action={action}> */}
+            <form>
+                <Input
+                    name="title"
+                    required
+                    placeholder="Title"
+                    type="text"
+                    // errors={state?.fieldErrors.title}
+                ></Input>
+                <Input
+                    name="description"
+                    required
+                    placeholder="Description"
+                    type="text"
+                    // errors={state?.fieldErrors.description}
+                ></Input>
+            </form>
         </div>
     );
 }
