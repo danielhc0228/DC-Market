9f3c56e287d54136176149297a75c86b869813dc
Daniel Chung <daniel20020228@gmail.com>
2025-07-14 16:59:58 +1000

    Add postSchema using zod and uploadPost function to upload post data to database

---
+++ app/(tabs)/life/add/actions.ts
@@ -0,0 +1,51 @@
+import db from "@/lib/db";
+import getSession from "@/lib/session";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import { z } from "zod";
+
+const postSchema = z.object({
+    title: z.string({
+        required_error: "Title is required",
+    }),
+    description: z.string({
+        required_error: "Description is required",
+    }),
+});
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export async function uploadPost(_: any, formData: FormData) {
+    const data = {
+        title: formData.get("title"),
+        description: formData.get("description"),
+    };
+
+    // parse error-less data
+    const result = postSchema.safeParse(data);
+    if (!result.success) {
+        return result.error.flatten();
+    } else {
+        // if there is no error, create new data to the database
+        const session = await getSession();
+        if (session.id) {
+            const post = await db.post.create({
+                data: {
+                    title: result.data.title,
+                    description: result.data.description,
+                    //connect the post to user who uploaded
+                    user: {
+                        connect: {
+                            id: session.id,
+                        },
+                    },
+                },
+                // after creation, only id is returned
+                select: {
+                    id: true,
+                },
+            });
+            revalidatePath("/life");
+            redirect(`/posts/${post.id}`);
+        }
+    }
+}
