e69edb526cc263c1a16e1ad2a27d4b5d851b978f
Daniel Chung <daniel20020228@gmail.com>
2025-07-18 15:55:19 +1000

    Add market logo images

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,19 @@
 import Link from "next/link";
 import "@/lib/db";
+import Image from "next/image";
 
 export default function Home() {
     return (
         <div className="flex min-h-screen flex-col items-center justify-between py-5">
             <div className="my-auto flex flex-col items-center gap-2 *:font-medium">
-                <span className="text-9xl">🥕</span>
-                <h1 className="text-4xl">Carrot</h1>
-                <h2 className="text-2xl">Welcome to Carrot Market!</h2>
+                <Image
+                    src={"/logo.png"}
+                    width={108}
+                    height={108}
+                    alt={"Logo"}
+                    className="object-cover"
+                />
+                <h2 className="text-2xl">Welcome to DC Market!</h2>
             </div>
             <div className="flex w-full flex-col items-center gap-3">
                 <Link href="/create-account" className="primary-btn py-2.5 text-lg">
